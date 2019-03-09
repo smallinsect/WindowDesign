56b4c2b548eb6eb7f0a948860024fdd2e57765ca
smallinsect <1161851688@qq.com>
2019-03-09 23:53:48 +0800

    第一个窗口

--- WindowDesign/WindowDesign/WindowDesignMain.cpp
+++ WindowDesign/WindowDesign/WindowDesignMain.cpp
@@ -1,9 +1,81 @@
-#include <iostream>
-using namespace std;
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	cout << "¹þ¹þ¹þ¹þ" << endl;
+//
+//	system("pause");
+//	return 0;
+//}
 
-int main() {
-	cout << "¹þ¹þ¹þ¹þ" << endl;
+#include <windows.h>
 
-	system("pause");
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPreInstance, LPTSTR lpCmdLine, int nCmdShow) {
+
+	//MessageBox(NULL, TEXT("°®°×²ËµÄÐ¡À¥³æ"), TEXT("À¥³æ"), MB_OK);
+	TCHAR szClassName[] = TEXT("my window");
+
+	MSG msg;
+	HWND hWnd;
+	WNDCLASS wc;
+
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wc.hInstance = hInstance;
+	wc.lpfnWndProc = WndProc;
+	wc.lpszClassName = szClassName;
+	wc.lpszMenuName = NULL;
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+
+	if (!RegisterClass(&wc)) {
+		MessageBox(NULL, TEXT("This program requires Windows NT!"), szClassName, MB_OK);
+		return 0;
+	}
+
+	hWnd = CreateWindow(szClassName,
+		TEXT("CLOCK"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL);
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
 	return 0;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam) {
+
+	HDC hdc;
+	PAINTSTRUCT ps;
+	RECT rect;
+
+	switch (Msg) {
+	case WM_PAINT:
+		hdc = BeginPaint(hWnd, &ps);
+
+		GetClientRect(hWnd, &rect);
+
+		DrawText(hdc, TEXT("°®°×²ËµÄÐ¡À¥³æ"), -1, &rect, 
+			DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, Msg, wParam, lParam);
 }
