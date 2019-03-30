b732fe8f2e9a2745697ff012d7aad8fce8570724
smallinsect <1161851688@qq.com>
2019-03-31 00:06:02 +0800

    socket客户端和服务端编程

---
+++ WindowDesign/SocketClient/SocketClientMain.cpp
@@ -0,0 +1,54 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int error;
+
+int main(){
+	int ret;
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0){
+		MessageBox(NULL, TEXT("WSAStartup error"), TEXT("tips"), MB_OK);
+		return -1;
+	}
+	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s == INVALID_SOCKET){
+		MessageBox(NULL, TEXT("create socket fail"), TEXT("tips"), MB_OK);
+		return -1;
+	}
+	SOCKADDR_IN addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(9000);//端口
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");//IP
+	//bind(s, (SOCKADDR*)&addr, sizeof(addr));
+
+	//连接
+	if (connect(s, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR){
+		MessageBox(NULL, TEXT("connect fail"), TEXT("message"), MB_OK);
+		return -1;
+	}
+
+	char buf[1024] = "爱白菜的小昆虫";
+	scanf("%s", buf);
+	//发送信息
+	if (send(s, buf, strlen(buf), 0) == SOCKET_ERROR){
+		MessageBox(NULL, TEXT("send message fail"), TEXT("message"), MB_OK);
+	}
+
+	//接受信息
+	if (recv(s, buf, sizeof(buf), 0) == SOCKET_ERROR){
+		MessageBox(NULL, TEXT("recv message fail"), TEXT("message"), MB_OK);
+	}
+	printf("%s\n", buf);
+	//关闭socket
+	if (closesocket(s) == SOCKET_ERROR){
+		MessageBox(NULL, TEXT("closesocket fail"), TEXT("message"), MB_OK);
+	}
+
+	WSACleanup();
+	system("pause");
+	return 0;
+}
---
+++ WindowDesign/SocketServer/SocketServerMain.cpp
@@ -0,0 +1,73 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <winsock2.h>
+
+#pragma comment(lib, "ws2_32.lib")
+
+int error;
+
+int main(){
+
+	int ret;
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0){
+		error = WSAGetLastError();
+		MessageBox(NULL, TEXT("WSAStartup error"), TEXT("tips"), MB_OK);
+		return -1;
+	}
+
+	SOCKET s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (s == INVALID_SOCKET){
+		error = WSAGetLastError();
+		MessageBox(NULL, TEXT("create socket fail"), TEXT("tips"), MB_OK);
+		return -1;
+	}
+
+	SOCKADDR_IN addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(9000);//端口
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");//IP
+
+	//给socket分配一个本地协议地址
+	if (bind(s, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR){
+		error = WSAGetLastError();
+		MessageBox(NULL, TEXT("bind fail"), TEXT("message"), MB_OK);
+		return -1;
+	}
+
+	//开始监听
+	if (listen(s, SOMAXCONN) == SOCKET_ERROR){
+		MessageBox(NULL, TEXT("listen fail"), TEXT("message"), MB_OK);
+		return -1;
+	}
+
+	while (true){
+		SOCKET s_new = accept(s, NULL, NULL);
+		if (s_new == SOCKET_ERROR){
+			MessageBox(NULL, TEXT("accept fail"), TEXT("tips"), MB_OK);
+			break;
+		}
+
+		char buf[1024];
+		memset(buf, 0, sizeof(buf));
+		ret = recv(s_new, buf, sizeof(buf), 0);
+		if (ret == 0 || ret == SOCKET_ERROR){
+			MessageBox(NULL, TEXT("recv fail"), TEXT("tips"), MB_OK);
+			break;
+		}
+		printf("%s\n", buf);
+
+		scanf("%s", buf);
+		if (send(s_new, buf, strlen(buf), 0) == SOCKET_ERROR){
+			MessageBox(NULL, TEXT("send fail"), TEXT("tips"), MB_OK);
+			break;
+		}
+	}
+
+	closesocket(s);
+	WSACleanup();
+
+	system("pause");
+	return 0;
+}
